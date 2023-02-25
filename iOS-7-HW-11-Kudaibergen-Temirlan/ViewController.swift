ae76b1bbe141be07eaaa7e89aea498e5e9c2c120
temirlan <k.temirlan.t@gmail.com>
2023-02-25 18:31:42 +0600

    Initial Commit

---
+++ iOS-7-HW-11-Kudaibergen-Temirlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-7-HW-11-Kudaibergen-Temirlan
+//
+//  Created by Темирлан Кудайберген on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
